1071cd2b70ad7a31ab1451ebb4d0dc83b29612b3
Santosh Prayaga <krishna.prayaga@imaginea.com>
2016-02-10 16:49:28 +0530

    Changed the Logic of Internet Failure Handling

--- src/main/java/com/imaginea/task/ExecutableClass.java
+++ src/main/java/com/imaginea/task/ExecutableClass.java
@@ -1,10 +1,12 @@
 package com.imaginea.task;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
 
 /**
  * @author Santosh
@@ -29,7 +31,15 @@ public class ExecutableClass {
 			LOG.info("--Given Page URL: " + pageURL + " and year to fetch mails is: " + year+" --");
 			CrawlLinks link = new CrawlLinks();
 			LOG.info("--Getting links by calling the method getWebLinks()--");
-			link.getWebLinks(pageURL, year, outFile, searchTag, searchAttr, connTryCount);
+			try {
+				Jsoup.connect("http://mail-archives.apache.org/mod_mbox/maven-users/")
+						.userAgent(
+								"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21")
+						.timeout(10000).execute();
+				link.getWebLinks(pageURL, year, outFile, searchTag, searchAttr, connTryCount);
+			} catch (IOException e) {
+				LOG.error("Cannot Connect to the URL: " + pageURL + "... Please check the Internet Connection");
+			}
 		} catch (Exception e) {
 			LOG.error("Excpetion in Main Class is:: " + e.getMessage());
 		}
